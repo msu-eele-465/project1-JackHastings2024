29e05c254d19ee28afc03e58d166085d7e573c04
Jack GG <jackhastingsgg@gmail.com>
2025-01-17 14:38:18 -0700

    Finished Code For Main and Added Flowchart for Main

--- app/main.asm
+++ app/main.asm
@@ -1,27 +1,8 @@
 ; Jack Glutting-Gilsdorf
 ; 1/17/2025
-; Project 1
-;  MSP430FR235x Demo - Toggle P1.0 using software
+; Project 1 - Toggle P1.0 using delay loop subroutine
 ;
-;  Description: Toggle P1.0 every 0.1s using software.
-;  By default, FR235x select XT1 as FLL reference.
-;  If XT1 is present, the PxSEL(XIN & XOUT) needs to configure.
-;  If XT1 is absent, switch to select REFO as FLL reference automatically.
-;  XT1 is considered to be absent in this example.
-;  ACLK = default REFO ~32768Hz, MCLK = SMCLK = default DCODIV ~1MHz.
-;
-;           MSP430FR2355
-;         ---------------
-;     /|\|               |
-;      | |               |
-;      --|RST            |
-;        |           P1.0|-->LED
-;
-;   Cash Hao
-;   Texas Instruments Inc.
-;   November 2016
-;   Built with Code Composer Studio v6.2.0
-;******************************************************************************
+
             .cdecls C,LIST,"msp430.h"  ; Include device header file
 ;-------------------------------------------------------------------------------
             .def    RESET                   ; Export program entry-point to
@@ -49,7 +30,7 @@ init:
 main:
 
             mov.w #1000, R14                ; set delay_ms to delay for 1000 ms (1 s)
-            call #delay_ms
+            call #delay_loop
 
             xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 0.1s
 
@@ -61,7 +42,7 @@ main:
 ;
 ; Inputs:
 ;   
-delay_ms: 
+delay_loop:
 
 ; inner loop timing calculation:
 ;   3 cycles per iteration * 332 iterations = 996 cycles
@@ -69,7 +50,7 @@ delay_ms:
 ;   996 + 2 = 998 --> need two extra nop cycles
 
                 ; setup inner loop counter
-delay_ms_outer  mov.w #332, R15             ; 2 cycles
+delayloop_outer  mov.w #332, R15             ; 2 cycles
 
                 ; delay a couple cycles to get timing exact
                 ; TODO: need to verify timing with a scope
@@ -77,12 +58,12 @@ delay_ms_outer  mov.w #332, R15             ; 2 cycles
                 nop                         ; 1 cycle
 
                 ; decrement inner loop variable
-delay_ms_inner  dec.w R15                   ; 3 cycles per iteration
-                jnz delay_ms_inner
+delayloop_inner  dec.w R15                   ; 3 cycles per iteration
+                jnz delayloop_inner
 
                 ; decrement outer loop variable
                 dec.w R14
-                jnz delay_ms_outer
+                jnz delayloop_outer
 
                 ret
 
